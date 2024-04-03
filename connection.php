99a85475d4553f24bf5a92dfdc86bcada2c5e5b4
Lethinhz <133493415+Lethinhz@users.noreply.github.com>
2024-04-03 11:36:39 +0700

    commit

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','online_shopping')
+$conn = mysqli_connect('mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','dflq5l7lgy864njo','ij51qki77kv7zr4x','d1v1s661ptw4c8sd')
  or die("Can not connect database".mysqli_connect_error());
 ?>
